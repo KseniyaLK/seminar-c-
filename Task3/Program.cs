ca717c949202387f9d7d73667282722af473918a
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-29 16:18:44 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 3. написать программу, которая на вход принимает число
+//  и выдает является ли число четным
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) 
+{
+    Console.WriteLine("Введенное число - четное!");
+}
+else 
+{
+    Console.WriteLine("Введенное число - не четное!");
+}
+
+ 
+ //bool result = n % 2 == 0;
+ //Console.WriteLine(result ? "число четное" : "число не четное");
