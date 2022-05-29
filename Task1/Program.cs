2584a0aa94f621c3588dc29f74c13d00cc27479e
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-29 16:17:57 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//1. написать программу, которая на вход принимает два числа, 
+// а на вывод выдает какое из них большее, какое меньшее 
+
+Console.Write("Введите первое число: ");
+int n1 = int.Parse (Console.ReadLine ());
+Console.Write("Введите второе число: ");
+int n2 = int.Parse (Console.ReadLine ());
+if (n1 > n2)
+{
+    Console.WriteLine("Максимальное число = "+n1);
+    Console.WriteLine("Минимальное число = "+n2);
+
+}
+else 
+{
+    Console.WriteLine("Максимальное число = "+n2); 
+    Console.WriteLine("Минимальное число = "+n1); 
+}
+    
