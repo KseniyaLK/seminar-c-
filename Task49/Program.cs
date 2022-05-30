336a4864ff30af8720005a6b5b76aa8ba2ae0c84
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-30 22:19:54 +0300

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int [a,b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(0, 10);
+            if(i%2 ==0 && j%2 ==0)
+            {
+                array[i,j] = array[i,j]*array[i,j];
+            }
+        }
+    } 
+    return array;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    int[,] array = new int [a,b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(0, 10);
+            if(i%2 ==0 && j%2 ==0)
+            {
+                array[i,j] = array[i,j]*array[i,j];
+            }
+        }
+    } 
+    return array;
+}
+
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// int[,] ChangeArray(int a, int b)
+// {
+//     int[,] array = new int [a,b];
+//     Random rndNum = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rndNum.Next(0, 10);
+//         }
+//     } 
+//     return array;
+// }
+
+
+int[,] arr = GetArray(5, 7);
+PrintArray(arr);
+ChangeArray(int a, int b)
+PrintArray(arr);
