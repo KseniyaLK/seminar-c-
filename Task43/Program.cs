0a964204d67e96980a15a175d0305b0e276da046
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-31 21:56:09 +0300

    Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -5,19 +5,37 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 =
 // 9 -> (-0,5; 5,5)
 
-Console.WriteLine("Введите значение b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите значение k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите значение b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите значение k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
 
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
 
-Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k1 * x + b1;
+
+// Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
 
 
+Console.WriteLine("Какое колличество чисел Вы хотите ввести?");
+int quantityNumber = int.Parse(Console.ReadLine());
+int [] array = new int [quantityNumber];
+//int calculateNumbers = 0;
+
+/// Метод заполнения массива значениями пользователя
+int FillArrayUserNumbers (int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число:");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return number;
+}
+
+FillArrayUserNumbers (quantityNumber);
