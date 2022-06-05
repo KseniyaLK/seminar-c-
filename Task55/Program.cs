a12f519f80563b65248ecdad883c49f8885c3316
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-06-05 22:06:57 +0300

    Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -3,35 +3,37 @@
 // невозможно, программа должна вывести сообщение для
 // пользователя.
 
-int[,] GetArray(int a, int b)
+
+
+/// Создание случайного двумерного массива
+int [,] GetArray(int rows, int columns)
 {
-    int[,] array = new int [a,b];
-    Random rndNum = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = rndNum.Next(0, 10);
-        }
-    } 
+    int [,] array = new int[rows, columns];
+    Random arrayRandomValue = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = arrayRandomValue.Next(1, 10);
+                }
+            }
     return array;
 }
 
+/// Метод печати массива
+void PrintArray(int[,] array)
 
-void PrintArray (int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
+
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write("{0,6}", array[i, j]);
         }
         Console.WriteLine();
     }
 }
-
-int[,] arr = GetArray(5, 7);
-
 
 Console.WriteLine("Введите колличесвто строк: ");
 int userRow = int.Parse(Console.ReadLine());
