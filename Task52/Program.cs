975448ada3d785ac3b31b0dddfd725b7a42f50b6
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-06-05 22:06:40 +0300

    Task50

---
+++ Task52/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+/// Создание случайного двумерного массива
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random arrayRandomValue = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = arrayRandomValue.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+/// Метод печати массива
+void PrintArray(int[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+
+        {
+            Console.Write("{0,6}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] arr = GetArray(3, 3);
+PrintArray(arr);
+
+
+
+int ArithmeticMean(int[,]array)
+{
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum +=array[i,j]; 
+        }
+        result = sum / array.GetLength(1);
+        //break;
+    }
+    return result;
+}
+
+int result = ArithmeticMean(arr);
+Console.WriteLine();
+Console.WriteLine(result);
