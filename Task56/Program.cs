25e9bc0f854f7ef4c7d3da45e03a32039091ab00
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-06-07 23:12:28 +0300

    Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
+
+// создание массива
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+// вывод массива на печать
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr = GetArray(5, 5);
+PrintArray(arr);
+Console.WriteLine();
+
+
+
+int MaxSumRow(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+
+        }
+
+        if (sum > result)
+        {
+            result = sum;
+        }
+        else result = result;
+
+    }
+
+    return result;
+}
+
+int rez = MaxSumRow(arr);
+Console.WriteLine(rez);
