35046a34d31d0ebc0dc0ab7e9966f532cdd03d2d
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-06-07 23:12:38 +0300

    Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,14 @@
+﻿//задача63
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+NaturalNumber(n);
+Console.ReadKey();
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num-1);
+    Console.Write(num + " ");
+}
+
